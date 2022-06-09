8b783087ff9fc623cd9eaf503496266d2160bab6
lbplatas <lbplatas@LBPLATAS10.indra.es>
2022-06-09 10:21:47 +0200

    Segunda versión de código

--- api-core/src/main/java/es/qc/api/apicore/ApiCoreApplication.java
+++ api-core/src/main/java/es/qc/api/apicore/ApiCoreApplication.java
@@ -2,12 +2,13 @@ package es.qc.api.apicore;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ImportResource;
 
 @SpringBootApplication
+@ImportResource("classpath:META-INF/**/applicationContext.xml")
 public class ApiCoreApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ApiCoreApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ApiCoreApplication.class, args);
+    }
 }
